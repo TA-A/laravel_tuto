91d9a7d12322f370394b7f83d9c277b34fbce091
Boot-Linux-Port-0418 <Vous@exemple.com>
2019-02-23 18:11:10 +0100

    tuto

---
+++ test/resources/views/about.blade.php
@@ -0,0 +1,18 @@
+@extends('layout')
+
+@section('title', 'About')
+
+@section('content')
+
+    <h1>About us</h1>
+
+    <p>Voluptate excepteur qui eu veniam voluptate tempor qui dolore.
+    Nisi cillum ex tempor laboris fugiat cillum ut. Est qui anim minim
+    ex pariatur laborum mollit ex excepteur ea amet sunt dolore. Irure
+    ipsum aliquip qui dolore sunt mollit cupidatat quis ex. Sint incididunt
+    aute do consequat fugiat sint veniam commodo ut proident esse mollit
+    proident. Officia eu do anim mollit cupidatat reprehenderit non sint
+    tempor quis tempor culpa proident irure. Commodo voluptate non qui
+    sit nulla duis incididunt.</p>
+
+@endsection
---
+++ test/resources/views/welcome.blade.php
@@ -0,0 +1,19 @@
+@extends('layout')
+
+@section('title', 'Laracasts')
+
+@section('content')
+
+    <h1>My {{ $foo }} website</h1>
+
+    {{-- <ul>
+
+        @foreach ($tasks as $task)
+
+            <li>{{ $task }}</li>
+
+        @endforeach
+
+    </ul> --}}
+
+@endsection
